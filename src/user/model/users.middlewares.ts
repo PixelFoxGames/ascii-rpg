d1d8131abdc7ca50b9a25e1f00a4b179f6ca7130
Tiago Lok <strife.tiago@gmail.com>
2020-09-20 23:36:16 -0300

    [ASC-1] inventory system — added license file

--- src/user/model/users.middlewares.ts
+++ src/user/model/users.middlewares.ts
@@ -4,7 +4,7 @@ import InventoriesService from "../../inventory/inventories.service";
 
 export default class UsersMiddlewares {
   static get save(): (next: HookNextFunction) => Promise<any> {
-    return async function(this: IUserDocument, next: HookNextFunction) {
+    return async function (this: IUserDocument, next: HookNextFunction) {
       this.inventory = this.isNew ? await InventoriesService.getByUserID(this.user_id) : await this.inventory.save();
       next();
     };
